ab247a76e8aa9fd3e9d6a6996d2cdc6aad82b418
slawekmarc <slawomir.marcjanski@rb.com>
2021-12-21 17:03:06 +0100

    Initial commit - very simple merge with static methods

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # gitleaks-config-helper
+
+Very simple C# (.Net 5) code that allows to merge two *.toml files into one.
+
+This tool's goal is to allow enterprise-scale deployment of GitLeaks: https://github.com/zricethezav/gitleaks/
+
+Every project is different and requires custom configuration. But you also want to have ability to fix issues or introduce new rules in central point
+
+With this tool you can run following logic in your CI/CD pipeline 
+
+1. Get base file shared with every project
+2. Create local rules or override rules from base file (using same id)
+3. Generate compiled final file for your project.
+
+Or.
+
+1. Create config file for each project type 
+2. Merge several files into one depending on how your solution is constructed
+
+Hope it will help somebody.
+
---
+++ src/gitleaks-config-helper/gitleaks-config-helper/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+using Tommy;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length < 4)
+            {
+                Console.WriteLine("Parameters not set");
+                Console.WriteLine("Usage: gitleaks-config-helper merge base_file merge_file out_file");
+                Console.WriteLine("Exiting");
+                return -1;
+            }
+            if (VerifyInput(args[1], args[2], args[3]))
+            {
+                try
+                {
+                    switch (args[0])
+                    {
+                        case "merge":
+                            MergeConfigs(args[1], args[2], args[3]);
+                            break;
+                        default:
+                            Console.WriteLine("Usage: gitleaks-config-helper merge base_file merge_file out_file");
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error while merging: " + e.Message);
+                    Console.WriteLine("Exiting");
+                    return -1;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Exiting");
+                return -1;
+            }
+            return 0;
+        }
+
+        public static void MergeConfigs(string baseConfig, string childConfig, string outFile)
+        {
+            // Parse files into a node
+            using (StreamReader readerBase = File.OpenText(baseConfig))
+            using (StreamReader readerChild = File.OpenText(childConfig))
+            {
+                // Parse the base and child table
+                TomlTable tblBase = TOML.Parse(readerBase);
+                TomlTable tblChild = TOML.Parse(readerChild);
+
+                //Merge tables 
+                foreach (TomlNode childRow in tblChild["rules"])
+                {
+                    for (int i = 0; i < tblBase["rules"].ChildrenCount; i++)
+                    {
+                        if (childRow["id"].ToString() == tblBase["rules"][i]["id"])
+                        {
+                            //Key found in base table can be replaced with new one
+                            tblBase["rules"].Delete(i);
+                            break;
+                        }
+                    }
+                    //Add new key
+                    tblBase["rules"].Add(childRow);
+                }
+
+                using (StreamWriter writer = File.CreateText(outFile))
+                {
+                    tblBase.WriteTo(writer);
+                    writer.Flush();
+                }
+            }
+        }
+
+        public static bool VerifyInput(string baseConfig, string childConfig, string outFile)
+        {
+            //Check if base file exists
+            if (!File.Exists(baseConfig))
+            {
+                Console.WriteLine("Base file does not exist: " + baseConfig);
+                return false;
+            }
+            //Check if child file exists
+            if (!File.Exists(childConfig))
+            {
+                Console.WriteLine("Child file does not exist: " + childConfig);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
